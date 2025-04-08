b354b8b3abfc62a33db6b3839ba19d9c1b2ad6de
culdeveloper <development@consultolab.com>
2025-04-08 12:27:45 +0300

    New send mail

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,6 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint', 'vue3-carousel-nuxt', 'nuxt-nodemailer'],
-	nodemailer: {
-		from: process.env.SMTP_EMAIL,
-		host: process.env.SMTP_HOST,
-		port: process.env.PORT,
-		auth: {
-			user: process.env.SMTP_USER,
-			pass: process.env.SMTP_PASSWORD,
-		},
-	},
 	carousel: {
 		prefix: 'Portfolio',
 	},
--- server/api/contact.ts
+++ server/api/contact.ts
@@ -1,6 +1,7 @@
+import { createError, defineEventHandler, readBody } from 'h3';
+import nodemailer from 'nodemailer';
+
 export default defineEventHandler(async (event) => {
-	const { sendMail } = useNodeMailer();
-	const config = useRuntimeConfig();
 	const body = await readBody(event);
 
 	if (!body.name || !body.email || !body.message) {
@@ -19,18 +20,33 @@ export default defineEventHandler(async (event) => {
 		});
 	}
 
-	const response = await sendMail({
-		subject: 'Contact form submission',
-		text: `Name: ${body.name}\nEmail: ${body.email}\nMessage: ${body.message}`,
-		to: config.nodemailer.from,
-	});
+	// Set up Nodemailer transporter
+	const transporter = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	} as nodemailer.TransportOptions);
 
-	if (!response) {
+	// Setup mail options
+	const mailOptions = {
+		from: process.env.SMTP_EMAIL,
+		to: process.env.SMTP_EMAIL, // The email to send the message to
+		subject: 'Contact Form Submission',
+		text: `Name: ${body.name}\nEmail: ${body.email}\nMessage: ${body.message}`,
+	};
+
+	try {
+		// Send the email
+		await transporter.sendMail(mailOptions);
+		return { message: 'Your message has been sent successfully!' };
+	} catch (error) {
+		console.error('Error sending email:', error);
 		throw createError({
 			statusCode: 500,
-			statusMessage: 'Failed to send email. Try again later.',
+			statusMessage: 'Failed to send email',
 		});
 	}
-
-	return { message: 'Your message has been sent successfully!' };
 });
